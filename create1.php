d59e02f2864b282bf3122d9bb42b9446420aec1a
ClariceOliveira07 <claricerodrigues071321@gmail.com>
2026-05-10 16:03:18 -0300

    Add files via upload

---
+++ create1.php
@@ -0,0 +1,33 @@
+<?php
+require_once ('../conexao.php');
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['nome'])) {
+    $stmt = $conn->prepare("INSERT INTO funcionarios (nome, dt_nasc) VALUES (:nome, :dt_nasc)");
+    $stmt->bindValue(':nome', $_POST['nome']);
+    $stmt->bindValue(':dt_nasc', $_POST['dt_nasc']);
+    $stmt->execute();
+    header('Location: index2.php');
+    exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Adicionar Funcionários</title>
+</head>
+<body>
+    <h1>Adicionar Funcionários</h1>
+    <form method="post">
+        <label for="nome">Nome completo:</label>
+        <input type="text" name="nome" placeholder="Nome" required>
+        <br>
+        <label for="dt_nasc">Data de Nascimento:</label>
+        <input type="date" name="dt_nasc">
+        <br>
+        <button type="submit">Adicionar</button>
+    </form>
+    <a href="index2.php">Voltar</a>
+</body>
+</html>
---
+++ delete1.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once ('../conexao.php');
+
+if(isset($_GET['id'])) {
+    $stmt = $conn->prepare("DELETE FROM funcionarios WHERE id = :id");
+    $stmt->bindValue(':id', $_GET['id']);
+    $stmt->execute();
+    header('Location: index2.php');
+    exit();
+}
+
+?>
