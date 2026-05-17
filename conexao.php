79e7cf75c8b0f2f8a0b271d476a8a336120dad49
ClariceOliveira07 <claricerodrigues071321@gmail.com>
2026-05-17 17:19:43 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -1,11 +1,11 @@
 <?php
-$host = 'localhost';
-$db = 'project_pw3crud';
-$user = 'root';
-$pass = ''; 
+$host = getenv('DB_HOST') ?: 'banco'; 
+$dbname = getenv('DB_NAME') ?: 'meu_crud_db';
+$user = getenv('DB_USER') ?: 'root';
+$pass = getenv('DB_PASS') ?: ' ';
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("Erro ao conectar: " . $e->getMessage());
