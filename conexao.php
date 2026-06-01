586eb5eb5f0bf7ebd9693bf8c84835d392faddab
ClariceOliveira07 <claricerodrigues071321@gmail.com>
2026-06-01 20:21:57 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 $host = getenv('DB_HOST') ?: '127.0.0.1'; 
 $dbname = getenv('DB_NAME') ?: 'project_pw3crud';
 $user = getenv('DB_USER') ?: 'root';
-$pass = getenv('DB_PASS') ?: ' ';
+$pass = getenv('DB_PASS') ?: '';
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
